a8b8e39f9fd7e64fc18e3c68970dfd7073ed7ee7
Dragos Cantemir <dcantemir@gmail.com>
2014-01-29 01:12:22 +0200

    Fix start-mongod batch script

--- start-mongod.cmd
+++ start-mongod.cmd
@@ -1,12 +1,12 @@
 @echo off
 setlocal
 
-set MONGOHQ_URL=mongodb://biker:6423km@troup.mongohq.com:10041/bikeusa
-
-echo Starting MongoDB server...
+rem echo Starting MongoDB server...
+rem set MONGO_BIN_DIR=C:\mongodb\bin
 rem set MONGO_DATA_DIR=C:\mongodb\data
 rem "%MONGO_BIN_DIR%\mongod.exe" -dbpath "%MONGO_DATA_DIR%"
 
+echo Starting MongoDB service...
 net start MongoDB
 
 if errorlevel 1 (
